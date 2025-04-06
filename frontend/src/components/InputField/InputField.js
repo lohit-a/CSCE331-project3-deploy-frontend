4567d52bde5d575b52e6f8ea09051a7bfa1934a1
HappyBombz <saved131@hotmail.com>
2025-04-06 02:14:03 -0500

    Inventory Management Page

--- frontend/src/components/InputField/InputField.js
+++ frontend/src/components/InputField/InputField.js
@@ -3,17 +3,20 @@ import './InputField.css';
 
 const InputField = ({ value, onChange, itemName, type = "text", placeholder, text }) => {
   return (
-    <div>
-    <h3>{text}</h3>
-    <input
-      type={type}
-      value={value || ''}
-      onChange={(e) => onChange(itemName, e)}
-      placeholder={placeholder}
-    />
+    <div className="input-field-container">
+      <label htmlFor={itemName} className="input-label">{text}</label>
+      <input
+        id={itemName}
+        className="input-box"
+        type={type}
+        value={value || ''}
+        onChange={(e) => onChange(itemName, e)}
+        placeholder={placeholder}
+        step="0.01"
+      />
     </div>
-    
   );
 };
 
 export default InputField;
+
