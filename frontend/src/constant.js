9d6d75cf18079fad48722c7aa891dde9c13c09e4
Lohitaksh Allampalli <141296578+lohit-a@users.noreply.github.com>
2025-04-30 00:55:39 -0500

    Changed SERVER URL to point to deployed backend

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,3 +1,3 @@
 export const WeatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
-export const SERVER_URL = 'http://localhost:8081';
-//export const SERVER_URL = 'https://proj3-t62-backenddeploy-production.up.railway.app';
+// export const SERVER_URL = 'http://localhost:8081';
+export const SERVER_URL = 'https://proj3-t62-backenddeploy-production.up.railway.app';
